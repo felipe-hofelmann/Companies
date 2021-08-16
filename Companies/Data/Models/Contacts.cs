d99b53105b6870fdf822ddaae9d4c8269645481b
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-16 14:21:17 -0300

    Contacts Model Created

---
+++ Companies/Data/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Contacts : Base
+    {
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string LinkedIn { get; set; }
+    }
+}
