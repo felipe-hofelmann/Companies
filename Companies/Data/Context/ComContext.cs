25d2da3589df7819f7c4da987c66a04e9e22187c
Felipe Hofelmann <felipehofelmann21@gmail.com>
2021-08-16 15:54:47 -0300

    Create Table Addresses Closed #20

--- Companies/Data/Context/ComContext.cs
+++ Companies/Data/Context/ComContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace Data.Context
 {
     public class ComContext : DbContext
     {
+        public DbSet<Address> Addresses { get; set; }
         public ComContext(): base ("Data Source=192.168.0.184;Initial Catalog=Companies;Persist Security Info=True;User ID=Companies;Password=@devlabs1")
         {
 
---
+++ Companies/Data/Migrations/202108161852058_CreateTableAddress.cs
@@ -0,0 +1,30 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableAddress : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Number = c.String(),
+                        Street = c.String(),
+                        Complement = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Addresses");
+        }
+    }
+}
