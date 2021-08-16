e2c6f4cd96118e369fd565e477c5d6cfa52448ab
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-16 16:32:01 -0300

    Table Companies & Jobs created

--- Companies/Data/Models/Company.cs
+++ Companies/Data/Models/Company.cs
@@ -9,7 +9,7 @@ namespace Data.Models
     public class Company : Base
     {
         public Profile Profile { get; set; }
-        //public Jobs Jobs { get; set; }
+        public Jobs Jobs { get; set; }
         //public Contacts Contact { get; set; }
         //public Address Address { get; set; }
     }
