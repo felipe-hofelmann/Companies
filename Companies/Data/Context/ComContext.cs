4493eb8ebd9386b0d0304f6293d93eac9c59be16
caio <caio.mateus.r@gmail.com>
2021-08-16 15:48:18 -0300

    fixed

--- Companies/Data/Context/ComContext.cs
+++ Companies/Data/Context/ComContext.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Data.Context  
 {
-    class ComContext : DbContext
+    public class ComContext : DbContext
     {
         public ComContext(): base ("Data Source=192.168.0.184;Initial Catalog=Companies;Persist Security Info=True;User ID=Companies;Password=@devlabs1")
         {
