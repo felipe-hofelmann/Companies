7e3fd3ab647f6160596447b8e43bf833cb3edce6
Felipe Hofelmann <felipehofelmann21@gmail.com>
2021-08-16 14:31:00 -0300

    Create Address Closed #1

--- Companies/Data/Models/Address.cs
+++ Companies/Data/Models/Address.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    public class Address
+    public class Address : Base
     {
         public string Number { get; set; }
         public string Street { get; set; }
