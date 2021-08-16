35c40bb4b9102e456cfe1db06fcf3ebb417f4625
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-16 16:26:56 -0300

    Create Contacts Table Closed #19

--- Companies/Data/Context/ComContext.cs
+++ Companies/Data/Context/ComContext.cs
@@ -14,6 +14,7 @@ namespace Data.Context
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Skills> Skills { get; set; }
         public DbSet<Jobs> Jobs { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }
         public ComContext(): base ("Data Source=192.168.0.184;Initial Catalog=Companies;Persist Security Info=True;User ID=Companies;Password=@devlabs1")
         {
 
