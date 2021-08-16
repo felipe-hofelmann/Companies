44504fb0b69527eec818e329e045bf392218f2c9
Adilson <sr.adilson@gmail.com>
2021-08-16 16:16:07 -0300

    RelationShip Jobs & Skills

---
+++ Companies/Data/Migrations/202108161915221_CreateRelationShipJobsSkills.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateRelationShipJobsSkills : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Skills", "Jobs_Id", c => c.Int());
+            CreateIndex("dbo.Skills", "Jobs_Id");
+            AddForeignKey("dbo.Skills", "Jobs_Id", "dbo.Jobs", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Skills", "Jobs_Id", "dbo.Jobs");
+            DropIndex("dbo.Skills", new[] { "Jobs_Id" });
+            DropColumn("dbo.Skills", "Jobs_Id");
+        }
+    }
+}
